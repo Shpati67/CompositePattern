6e722660f88506c18f06e29e2abb79a2fb0f6c4b
shpat67 <sg6767@g.rit.edu>
2023-11-30 11:13:54 +0100

    adding unit test file

---
+++ CompositePatternTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CompositePatternTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
